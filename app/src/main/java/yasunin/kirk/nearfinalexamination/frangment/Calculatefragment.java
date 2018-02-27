5483f5b33b916095536d86d4f57baec5733c9c3c
yasunin <yasunin09@gmail.com>
2018-02-27 11:19:58 +0700

    Fished

--- app/src/main/java/yasunin/kirk/nearfinalexamination/frangment/Calculatefragment.java
+++ app/src/main/java/yasunin/kirk/nearfinalexamination/frangment/Calculatefragment.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import yasunin.kirk.nearfinalexamination.MainActivity;
 import yasunin.kirk.nearfinalexamination.R;
@@ -37,7 +40,27 @@ public class Calculatefragment extends Fragment {
         });
 
         //        Exchang
+        Button button = getView().findViewById(R.id.btnExchang);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = getView().findViewById(R.id.edtmoney);
+                String moneyString = editText.getText().toString().trim();
 
+                if (moneyString.isEmpty()) {
+                    Toast.makeText(getActivity(), "Please Fill USD money", Toast.LENGTH_SHORT).show();
+                }else{
+
+                    double moneyAdouble = Double.parseDouble(moneyString);
+                    double answerAdouble = moneyAdouble * 33;
+                    String answerString  = Double.toString(answerAdouble);
+
+                    Toast.makeText(getActivity(),"Your"+moneyString+"USD===>"+answerString+"THB",
+                            Toast.LENGTH_SHORT).show();
+                    editText.setText("");
+                }
+            }
+        });
 
     } //Main Method
 
